0c3ecaa67b14521fe4a94b8f4e95c5e147b0e852
liuguoqing0814 <2583606693@qq.com>
2019-01-10 15:11:54 +0800

    修改

--- back-stage/js/script.js
+++ back-stage/js/script.js
@@ -1,36 +1,39 @@
-var lineChartData = {
-	//X坐标数据
-		labels:["周一","周二","周三","周四","周五","周六","周末"],
-		datasets:[
-				{
-				//统计表的背景颜色
-					fillColor:"#ddd",
-				//统计表画笔颜色
-					strokeColor:"#f60",
-				//点的颜色
-					pointColor:"#f60",
-				//点边框的颜色
-					pointStorkeColor:"red",
-				//鼠标触发时点的颜色
-					pointHighlightFill:"red",
-				//鼠标触发时点边框的颜色
-					pointHighlightStorke:"#000",
-				//Y坐标数据
-					data:[300,555,655,714,899,905,1000]
-				},
-				{
-					fillColor:"rgba(255,255,255,,0)",
-					strokeColor:"rgba(92,184,92,1)",
-					pointColor:"rgba(23,126,23,1)",
-					pointStorkeColor:"#fff",
-					pointHighlightFill:"#fff",
-					pointHighlightStorke:"rgba(151,187,205,1)",
-					data:[314,455,755,814,999,1000]
-				}
-		]
-}
-
-window.onload = function(){
-	var ctx = document.getElementById("canvas").getContext("2d");
-	window.myLine = new Chart(ctx,lineChartData);
-}
+option = {
+    title: {
+        text: '折线图堆叠'
+    },
+    tooltip: {
+        trigger: 'axis'
+    },
+    legend: {
+        data:['邮件营销','联盟广告']
+    },
+    toolbox: {
+        feature: {
+            saveAsImage: {}
+        }
+    },
+    xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: ['周一','周二','周三','周四','周五','周六','周日']
+    },
+    yAxis: {
+        type: 'value'
+    },
+    series: [
+        {
+            name:'邮件营销',
+            type:'line',
+            stack: '总量',
+            data:[120, 132, 101, 134, 90, 230, 210]
+        },
+        {
+            name:'联盟广告',
+            type:'line',
+            stack: '总量',
+            data:[220, 182, 191, 234, 290, 330, 310]
+        },
+       
+    ]
+};
